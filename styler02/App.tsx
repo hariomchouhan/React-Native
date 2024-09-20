9566893bc449767786f280d7d173ec7ae98b44d2
Hariom <hariomchouhan430@gmail.com>
2024-09-20 18:13:44 +0930

    design card with image in React Native

--- styler02/App.tsx
+++ styler02/App.tsx
@@ -11,6 +11,7 @@ const App = () => {
         <FlatCards />
         <ElevatedCards />
         <FancyCards />
+        <FancyCards />
       </ScrollView>
     </SafeAreaView>
   )
--- styler02/components/FancyCards.tsx
+++ styler02/components/FancyCards.tsx
@@ -29,14 +29,50 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         paddingHorizontal: 8
     },
-    card: {},
-    cardElevated: {},
-    cardImage: {
-        height: 180
+    card: {
+        width: 350,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 'auto',
     },
-    cardBody: {},
-    cardTitle: {},
-    cardLabel: {},
-    cardDescription: {},
-    cardFooter: {},
+    cardElevated: {
+        backgroundColor: '#FFFFFF',
+        elevation: 3,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        }
+    },
+    cardImage: {
+        height: 180,
+        marginBottom: 8,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
+    },
+    cardBody: {
+        flex: 1,
+        flexGrow: 1,
+        paddingHorizontal: 12,
+    },
+    cardTitle: {
+        color: '#000000',
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 4,
+    },
+    cardLabel: {
+        color: '#000000',fontSize: 14,
+        marginBottom: 6,
+    },
+    cardDescription: {
+        color: '#242B2E',
+        fontSize: 12,
+        marginBottom: 12,
+        marginTop: 6,
+        flexShrink: 1,
+    },
+    cardFooter: {
+        color: '#000000',
+    },
 })
