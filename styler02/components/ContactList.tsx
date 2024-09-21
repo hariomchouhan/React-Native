6671c3a7430b1b30b7262c1cad6acab77c72ba0e
Hariom <hariomchouhan430@gmail.com>
2024-09-21 17:42:18 +0930

    Design contact list like whatsApp chat style in React Native

--- styler02/components/ContactList.tsx
+++ styler02/components/ContactList.tsx
@@ -43,8 +43,10 @@ export default function ContactList() {
                 }}
                 style={styles.userImage}
                 />
+                <View>
                 <Text style={styles.userName}>{name}</Text>
                 <Text style={styles.userStatus}>{status}</Text>
+                </View>
             </View>
         ))}
       </ScrollView>
@@ -58,13 +60,31 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         paddingHorizontal: 8
     },
-    container: {},
-    userCard: {},
+    container: {
+        paddingHorizontal: 16,
+        marginBottom: 4,
+    },
+    userCard: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 3,
+        backgroundColor: '#3b8e4d',
+        padding: 8,
+        borderRadius: 10
+    },
     userImage: {
         height: 60,
         width: 60,
-        borderRadius: 60 / 2
+        borderRadius: 60 / 2,
+        marginRight: 14,
     },
-    userName: {},
-    userStatus: {},
+    userName: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFF',
+    },
+    userStatus: {
+        fontSize: 12,
+    },
 })
