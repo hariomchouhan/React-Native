5eaf8205854fccbd79d2a06b375d077fa8824df7
Hariom <hariomchouhan430@gmail.com>
2024-09-27 15:04:35 +0930

    Complete project of the intructor side and working fine

--- rollthedice/src/App.tsx
+++ rollthedice/src/App.tsx
@@ -1,5 +1,5 @@
-import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import type { PropsWithChildren } from 'react'
 
 import DiceOne from '../assets/One.png'
@@ -22,9 +22,44 @@ const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
 }
 
 export default function App(): JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne)
+
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne)
+        break;
+      case 2:
+        setDiceImage(DiceTwo)
+        break;
+      case 3:
+        setDiceImage(DiceThree)
+        break;
+      case 4:
+        setDiceImage(DiceFour)
+        break;
+      case 5:
+        setDiceImage(DiceFive)
+        break;
+      case 6:
+        setDiceImage(DiceSix)
+        break;
+    
+      default:
+        setDiceImage(DiceOne)
+        break;
+    }
+  }
   return (
-    <View>
-      <Text>Hariom Chouhan</Text>
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable 
+      onPress={rollDiceOnTap}
+      >
+        <Text style={styles.rollDiceBtnText}>Roll the Dice</Text>
+      </Pressable>
     </View>
   )
 }
