a4082bc776dba6a32cf7d1a841dc1a24fa2b0366
Hariom <hariomchouhan430@gmail.com>
2024-09-28 15:06:45 +0930

    Start Tic Tac Toe app and explore FontAwesome icons in React Native

---
+++ tictactoe07/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+      />
+      <View>
+        <Text>Hariom Chouhan</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
