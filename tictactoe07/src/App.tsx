1eb9fdf059c09c3db5f88f5bdcd0a4b0e64bfb1d
Hariom <hariomchouhan430@gmail.com>
2024-09-28 15:45:38 +0930

    Finishing tic tac toe in React Native

--- tictactoe07/src/App.tsx
+++ tictactoe07/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import {
+  FlatList,
+  Pressable,
   SafeAreaView,
   StatusBar,
   StyleSheet,
@@ -101,15 +103,122 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView>
-      <StatusBar
+      <StatusBar />
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View
+          style={[
+            styles.playerInfo,
+            isCross ? styles.playerX : styles.playerO
+          ]}>
+          <Text style={styles.gameTurnTxt}>
+            Player {isCross ? "X" : "O"}'s Turn
+          </Text>
+        </View>
+      )}
+
+      {/* Game Grid */}
+      <FlatList
+        numColumns={3}
+        data={gameState}
+        style={styles.grid}
+        renderItem={({ item, index }) => (
+          <Pressable
+            key={index}
+            style={styles.card}
+            onPress={() => onChangeItem(index)}
+          >
+            <Icons name={item} />
+          </Pressable>
+        )}
       />
-      <View>
-        <Text>Hariom Chouhan</Text>
-      </View>
+
+      {/* Game Action */}
+      <Pressable
+        style={styles.gameBtn}
+        onPress={reloadGame}
+      >
+        <Text style={styles.gameBtnText}>
+          {gameWinner ? "Start New Game" : "Reload The Game"}
+        </Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  playerInfo: {
+    height: 56,
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+});
 
 export default App;
