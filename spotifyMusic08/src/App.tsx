8d22e61517c936416d0517bbd8332d00918b6547
Hariom <hariomchouhan430@gmail.com>
2024-09-30 14:53:02 +0930

    Conditional rendering of UI for music in React Native

--- spotifyMusic08/src/App.tsx
+++ spotifyMusic08/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -9,9 +10,32 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { addTrack, setupPlayer } from '../musicPlayerServices';
 
 function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
 
+  async function setup() {
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+  useEffect(() => {
+    setup()
+  }, [])
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+  
   return (
     <SafeAreaView>
       <StatusBar
@@ -21,6 +45,10 @@ function App(): React.JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default App;
