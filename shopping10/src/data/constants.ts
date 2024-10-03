10db7009582ad1a6cc82558c77f09160bf0a33e4
Hariom <hariomchouhan430@gmail.com>
2024-10-04 01:46:38 +0930

    finish the project of shopping trending products in React Native

--- shopping10/src/data/constants.ts
+++ shopping10/src/data/constants.ts
@@ -55,4 +55,100 @@ export const PRODUCTS_LIST: Product[] = [
             '15.49 cm (6.1 inch) Super Retina XDR Display',
         ],
     },
+
+    {
+        id: '5',
+        name: 'APPLE iPhone 13 (Midnight, 128 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 69900,
+        discountPrice: 61999,
+        offerPercentage: 11,
+        rating: 4.7,
+        ratingCount: 102314,
+        tags: [
+            '12MP Front Camera',
+            '12MP Dual Rear Camera',
+            '15.49 cm (6.1 inch) Super Retina XDR Display',
+        ],
+    },
+    {
+        id: '6',
+        name: 'APPLE iPhone 12 (Black, 64 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 65900,
+        discountPrice: 54999,
+        offerPercentage: 16,
+        rating: 4.6,
+        ratingCount: 160972,
+        tags: [
+            '12MP Dual Rear Camera',
+            '15.49 cm (6.1 inch) Super Retina XDR Display',
+        ],
+    },
+    {
+        id: '7',
+        name: 'APPLE iPhone SE (Red, 128 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 43900,
+        discountPrice: 35999,
+        offerPercentage: 18,
+        rating: 4.5,
+        ratingCount: 75312,
+        tags: [
+            '12MP Rear Camera',
+            '11.94 cm (4.7 inch) Retina HD Display',
+        ],
+    },
+    {
+        id: '8',
+        name: 'APPLE iPhone 14 Plus (Yellow, 256 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 99900,
+        discountPrice: 86999,
+        offerPercentage: 13,
+        rating: 4.8,
+        ratingCount: 23841,
+        tags: [
+            '12MP Front Camera',
+            '12MP Dual Rear Camera',
+            '17.02 cm (6.7 inch) Super Retina XDR Display',
+        ],
+    },
+    {
+        id: '9',
+        name: 'APPLE iPhone 14 Pro (Space Black, 128 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 129900,
+        discountPrice: 117999,
+        offerPercentage: 9,
+        rating: 4.8,
+        ratingCount: 48432,
+        tags: [
+            '48MP Triple Rear Camera',
+            '12MP Front Camera',
+            'Dynamic Island',
+            '16.95 cm (6.7 inch) Super Retina XDR Display',
+        ],
+    },
+    {
+        id: '10',
+        name: 'APPLE iPhone 13 Pro (Sierra Blue, 256 GB)',
+        imageUrl:
+            'https://rukminim1.flixcart.com/image/300/400/k2jbyq80pkrrdj/mobile-refurbished/k/y/d/iphone-11-256-u-mwm82hn-a-apple-0-original-imafkg25mhaztxns.jpeg',
+        originalPrice: 119900,
+        discountPrice: 109999,
+        offerPercentage: 8,
+        rating: 4.8,
+        ratingCount: 39428,
+        tags: [
+            '12MP Front Camera',
+            '12MP Triple Rear Camera',
+            '15.5 cm (6.1 inch) Super Retina XDR Display',
+        ],
+    },
 ];
--- shopping10/src/screens/Home.tsx
+++ shopping10/src/screens/Home.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+// React Navigation
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+
+// Components
+import ProductItems from '../components/ProductItems'
+import Seperator from '../components/Seperator'
+
+// data
+import { PRODUCTS_LIST } from '../data/constants'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({navigation} :HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList 
+      data={PRODUCTS_LIST}
+      keyExtractor={item => item.id}
+      ItemSeparatorComponent={Seperator}
+      renderItem={({item}) => (
+        <Pressable
+        onPress={() => {
+            navigation.navigate("Details", {
+                product: item
+            })
+        }}
+        >
+            <ProductItems product={item} />
+        </Pressable>
+      )}
+      />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+  
+      padding: 12,
+      backgroundColor: '#FFFFFF',
+    },
+  });
