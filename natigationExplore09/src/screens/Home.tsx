d567754e43c13527b6c4383c978655a508e98e4f
Hariom <hariomchouhan430@gmail.com>
2024-10-03 20:52:31 +0930

    Setting up Home Screen for navigation in React Native

--- natigationExplore09/src/screens/Home.tsx
+++ natigationExplore09/src/screens/Home.tsx
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+// Navigation
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+const Home = ({navigation}: HomeProps) => {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <View style={styles.container} >
+      <Text style={styles.smallText}>Home Screen</Text>
+      <Button
+      title='Go to this'
+      // onPress={() => navigation.navigate("Details", {
+      //   productId: "86"
+      // })}
+
+      // onPress={() => navigation.navigate("Details")}
+
+      onPress={() => navigation.push("Details", {
+        productId: "86"
+      })}
+      />
     </View>
   )
 }
